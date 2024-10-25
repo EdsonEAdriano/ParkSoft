6a0da92a69ddb8bdc4ea5139eb75bd3530f08b58
SrBauer <bauer.jonathan.jj@gmail.com>
2024-10-25 20:25:08 -0300

    feature home page

---
+++ src/app/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="main-content">
+      <div className="text-container">
+        <h1 className="homeText">Bem-vindo ao</h1>
+        <h1 className="homeText soft-park">SOFT PARK</h1>
+      </div>
+    </div>
+  );
+}
