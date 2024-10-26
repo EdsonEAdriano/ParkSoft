529b724ceab23a9907c3b994b94e42a203dfe497
SrBauer <bauer.jonathan.jj@gmail.com>
2024-10-25 21:05:31 -0300

    ajuste da responcividade

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,10 +3,12 @@ import "./home.css";
 
 export default function Home() {
   return (
-    <div className="main-content">
-      <div className="text-container">
-        <h1 className="homeText">Bem-vindo ao</h1>
-        <h1 className="homeText soft-park">SOFT PARK</h1>
+    <div className="home-container">
+      <div className="main-content">
+        <div className="text-container">
+          <h1 className="homeText">Bem-vindo ao</h1>
+          <h1 className="homeText soft-park">SOFT PARK</h1>
+        </div>
       </div>
     </div>
   );
