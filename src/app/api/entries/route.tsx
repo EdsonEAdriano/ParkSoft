bcfad11cdd260c97375dd5089a89287c12ed4ca2
edson.adriano <edsoneurides0705@gmail.cpm>
2024-11-08 21:15:53 -0300

    Complete Entry CRUD

--- src/app/api/entries/route.tsx
+++ src/app/api/entries/route.tsx
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 
 
 interface Entry {
+  id: number;
   vehicleTypeID: string;
   brand: string;
   model: string;
@@ -15,10 +16,11 @@ interface Entry {
 
  
 export async function GET() {
+  const client = await db.connect();
   try {
-    const client = await db.connect();
     const { rows } = await client.sql`SELECT 
                                         ent.id,
+                                        veh.vehicle_type,
                                         veh.brand,
                                         veh.model,
                                         ent.plate, 
@@ -38,16 +40,19 @@ export async function GET() {
     return NextResponse.json({ entries: rows });
   } catch (error) {
     console.error('Error while getting entries:', error);
-
     return NextResponse.json({ error: "Error while getting entries" }, { status: 500 }); 
+  } finally {
+    client.release();
   }
 }
 
 export async function POST(request: Request) {
+  const client = await db.connect();
   try {
-    const client = await db.connect();
     const data: Entry = await request.json();
     const { vehicleTypeID, brand, model, plate, color, entryDate } = data;
+
+    console.log(data);
 
     let vehicleId: number;
 
@@ -77,5 +82,67 @@ export async function POST(request: Request) {
   } catch (error) {
     console.error('Error while creating entry:', error);
     return NextResponse.json({ error: "Error while creating entry" }, { status: 500 });
+  } finally {
+    client.release();
+  }
+}
+
+export async function PUT(request: Request) {
+  const client = await db.connect();
+  try {
+    const data: Entry = await request.json();
+    const { id, vehicleTypeID, brand, model, plate, color } = data;
+
+    let vehicleId: number;
+
+    const vehicleResult = await client.sql`
+      SELECT id FROM t_vehicles WHERE brand = ${brand} AND model = ${model}
+    `;
+
+    if (vehicleResult.rows.length > 0) {
+      vehicleId = vehicleResult.rows[0].id;
+    } else {
+      const insertResult = await client.sql`
+        INSERT INTO t_vehicles (vehicle_type, brand, model)
+        VALUES (${vehicleTypeID}, ${brand}, ${model})
+        RETURNING id
+      `;
+      vehicleId = insertResult.rows[0].id;
+    }
+    
+    await client.sql`
+      UPDATE t_entries 
+        SET vehicle_id = ${vehicleId}, plate = ${plate}, color = ${color}
+      WHERE id = ${id}
+    `;
+
+    return NextResponse.json({ message: "Entry updated successfully", vehicleId });
+  } catch (error) {
+    console.error('Error while updating entry:', error);
+    return NextResponse.json({ error: "Error while updating entry" }, { status: 500 });
+  } finally {
+    client.release();
+  }
+}
+
+
+export async function DELETE(request: Request) {
+  const client = await db.connect();
+  try {
+    const { id } = await request.json();
+
+    console.log('id da entrada', id)
+
+    await client.sql`
+      DELETE FROM t_entries 
+      WHERE id = ${id}
+    `;
+
+    return NextResponse.json({ message: "Entry deleted successfully" });
+  } catch (error) {
+    console.error('Error while deleting entry:', error);
+    return NextResponse.json({ error: "Error while deleting entry" }, { status: 500 });
+  } finally {
+    client.release();
   }
 }
