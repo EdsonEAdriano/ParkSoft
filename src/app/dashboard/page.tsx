060e4bfc25f613102d526bbab552d2392de613c5
Edsu <79771919+EdsonEAdriano@users.noreply.github.com>
2025-03-21 19:57:23 -0300

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Dashboard from './components/dashboard';
 
+
+
+// Dashboard
 export default function Page() {
   return (
     <div>
+      <Header />
       <Dashboard />
+      <Footer />
     </div>
   );
 }
